6b487a2f39964a01f16b24bc689c2be8f85588a6
shivanivasantha <101004511+shivanivasantha@users.noreply.github.com>
2022-05-16 17:54:48 +0530

    Add files via upload

---
+++ 7a.sh
@@ -0,0 +1,17 @@
+set -- `who`
+u=$1
+time=$4
+set -- `echo $time | tr ":" " "`
+hr=$1
+if [ $hr -ge 5 ] && [ $hr -lt 12 ]
+then
+	echo "Good morning($time) $u"
+elif [ $hr -ge 12 ] && [ $hr -lt 15 ]
+then
+	echo "Good afternoon($time) $u"
+elif [ $hr -ge 15 ] && [ $hr -lt 19 ]
+then
+	echo "Good evening($time) $u"
+else	
+	echo "Good night($time) $u"
+fi
